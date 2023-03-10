a6c37cad0f7480f783777f8a60fc3cf2674de500
shahidwaris <71723136+shahidwaris@users.noreply.github.com>
2023-03-10 17:43:51 +0530

    Create App.js

---
+++ OMDB_API/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import SearchBar from "./SearchBar";
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default App;
